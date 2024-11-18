ee8b65aa37d588233b2ab3afa53acfb650185bd5
AntRkv <rakovnyc@gmail.com>
2024-11-18 15:36:43 -0500

    Add JWT middleware for route protectiomUpd

--- server/server.mjs
+++ server/server.mjs
@@ -1,28 +1,30 @@
-const express = require("express");
-const mongoose = require("mongoose");
-const dotenv = require("dotenv");
-const cors = require("cors");
+import express from "express"
+import dotenv from "dotenv";
+import cors from "cors";
+import authRoute from './routes/authRoute.mjs'
+import connectDB from "./config/db.mjs"
 
 dotenv.config();
 const app = express();
+
+
+// MongoDB Connection
+connectDB();
+
 
 // Middleware
 app.use(cors());
 app.use(express.json());
 
-// MongoDB Connection
-mongoose
-  .connect(process.env.MONGO_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
-  .then(() => console.log("MongoDB Connected"))
-  .catch((err) => console.error(err));
+
+//Single endpoint just to test API. Send data to browser
+app.get('/', (req, res) => res.send('API Running'));
+
 
 // Routes
-app.use("/api/auth", require("./routes/auth"));
-app.use("/api/videos", require("./routes/videos"));
-app.use("/api/shorts", require("./routes/shorts"));
+app.use("/api/auth", authRoute);
+// app.use("/api/videos", require("./routes/videos"));
+// app.use("/api/shorts", require("./routes/shorts"));
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
