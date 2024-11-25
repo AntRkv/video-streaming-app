3fb4db693c1cdb38f531444307d3e570780e5004
AntRkv <rakovnyc@gmail.com>
2024-11-25 17:38:23 -0500

    Add route for ViewVideo in App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import UploadVideo from "./pages/UploadVideo";
+import ViewVideo from "./pages/ViewVideo";
 
 const App = () => {
   return (
@@ -9,6 +10,7 @@ const App = () => {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/upload" element={<UploadVideo />} />
+        <Route path="/videos/:id" element={<ViewVideo />} />
       </Routes>
     </Router>
   );
